550a9712e6cdc573c0c806478d1308fa2bdab62a
TMelnyk2021 <t.melnyk2021@gmail.com>
2025-02-15 19:21:36 +0200

    lisää sovelluksen testidatan

---
+++ src/components/App/testdata.js
@@ -0,0 +1,65 @@
+const testdata = [
+    {
+      id:           "1",
+      type:         "Kanala1",
+      total_amount: 90000,
+      collect_date: "2025-02-09",
+      s_amount:     300,
+      m_amount:     8300,
+      l_amount:     400,
+      bad_amount:   77
+    },
+    {
+      id:           "2",
+      type:         "Kanala2",
+      total_amount:  10000,
+      collect_date: "2025-02-09",
+      s_amount:      400,
+      m_amount:      9400,
+      l_amount:      200,
+      bad_amount:    115
+    },
+    {
+      id:            "3",
+      type:          "Kanala1",
+      total_amount:  11000,
+      collect_date: "2025-02-10",
+      s_amount:      450,
+      m_amount:      10100,
+      l_amount:      450,
+      bad_amount:    89
+    },  
+    {
+      id:            "4",
+      type:          "Kanala2",
+      total_amount:  99000,
+      collect_date: "2025-02-10",
+      s_amount:      200,
+      m_amount:      9500,
+      l_amount:      200,
+      bad_amount:    65
+    },
+    {
+      id:            "5",
+      type:          "Kanala3",
+      total_amount:  12000,
+      collect_date: "2025-02-11",
+      s_amount:      330,
+      m_amount:      11400,
+      l_amount:      270,
+      bad_amount:    77
+    },
+    {
+      id:            "6",
+      type:          "Kanala3",
+      total_amount:  10250,
+      collect_date: "2025-02-12",
+      s_amount:      150,
+      m_amount:      10000,
+      l_amount:      100,
+      bad_amount:    101
+    } 
+  ]
+  
+  export default testdata
+  
