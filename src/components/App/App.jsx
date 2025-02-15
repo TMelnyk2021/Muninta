1473152d881e8546d78394bb1958737164e8bccd
TMelnyk2021 <t.melnyk2021@gmail.com>
2025-02-15 14:32:59 +0200

    lisää Items-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,8 @@
+import Items from '../Items'
 import { ButtonContainer } from '../../shared/buttons'
 import { FloatingButton } from '../../shared/buttons'
 import Content from '../Content'
 import styles from './App.module.scss'
-import Item from '../Item'
 import Header from '../Header'
 import Menu from '../Menu'
 
@@ -16,11 +16,7 @@ function App() {
       <div className={styles.app}>
         <Header />
         <Content>
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <FloatingButton secondary>+</FloatingButton>
+        <Items />
         </Content>
         <Menu />
       </div>
