cf96a115e21cfb4e438cf6a70424285638aaa8ec
TMelnyk2021 <t.melnyk2021@gmail.com>
2025-02-17 00:20:38 +0200

    lisää tietojen kuljetuksen pääkomponentilta

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,7 +2,8 @@ import { MdNavigateNext } from 'react-icons/md'
 
 import styles from './Item.module.scss'
 
-function Item() {
+function Item({data, ...props}) {
+
 
     return (
 
@@ -12,19 +13,34 @@ function Item() {
         // 7700 - keskikokoisia M
         // 100 - suuria L
         // 80 - kuinka moni on huono kokonaismäärästä
-    <div className={styles.item}>
+
+      // Alla vanha koodi kommentoituna:
+      // <div className={styles.item_type}>Uusi kanala</div>
+      // <div className={styles.item_amount}>8000 munaa yhteensä</div> 
+      // <div className={styles.item_date}>11.2.2025</div> 
+      // <div className={styles.item_amount1}>200 S-munaa</div> 
+      // <div className={styles.item_amount1}>7700 M-munaa</div> 
+      // <div className={styles.item_amount1}>100 L-munaa</div>
+      // <div className={styles.item_amount2}>80 huonoa munaa</div>
+
+      //    <div className={styles.item_average}>? munaa/kk</div>
+
+      <div className={styles.item}>
+
       <div className={styles.item_data}>
-      <div className={styles.item_type}>Uusi kanala</div>
-      <div className={styles.item_amount}>8000 munaa yhteensä</div> 
-      <div className={styles.item_date}>11.2.2025</div> 
-      <div className={styles.item_amount1}>200 S-munaa</div> 
-      <div className={styles.item_amount1}>7700 M-munaa</div> 
-      <div className={styles.item_amount1}>100 L-munaa</div>
-      <div className={styles.item_amount2}>80 huonoa munaa</div>
-        </div>
+        <div className={styles.item_type}>{data.type}</div>
+        <div className={styles.item_amount}>{data.total_amount} munaa yhteensä </div>
+        <div className={styles.item_date}>{data.collect_date}</div>
+        <div className={styles.item_amount1}>{data.s_amount} S-pienet</div> 
+        <div className={styles.item_amount1}>{data.m_amount} M-keskikokoiset</div> 
+        <div className={styles.item_amount1}>{data.l_amount} L-isot</div> 
+        <div className={styles.item_amount2}>{data.bad_amount} huonoa munaa</div>
+      </div>
+
         <div className={styles.item_edit}>
         <MdNavigateNext />
       </div>
+
       </div>
     )
   }
