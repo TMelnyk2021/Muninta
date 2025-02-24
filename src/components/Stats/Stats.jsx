62bd23a6e3fb6642eda79967f41409a98166698c
TMelnyk2021 <t.melnyk2021@gmail.com>
2025-02-24 18:11:55 +0200

    lisää aikajanan piirtävän viivakaavion

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,10 +1,47 @@
+import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+
 import styles from './Stats.module.scss'
 
-function Stats() {
+function Stats(props) {
+
+  const locale = "fi-FI"
+
+  const numberFormat = new Intl.NumberFormat(locale)
+  
+  const linedata = props.data.map(
+    (item) => ({
+      date: new Date(item.collect_date).getTime(),
+      amount: item.m_amount
+    })
+  )
+
 
   return (
     <div className={styles.stats}>
-      <h2>Stats</h2>
+
+   <h2>Tilastot</h2>
+
+   <h3>Kerätyt M-keskikokoisetmunat aikajanalla</h3>
+   <ResponsiveContainer height={350}>
+        <LineChart data={linedata}>
+          <Line dataKey='amount' />
+          <XAxis type='number'
+                 dataKey='date'
+                 domain={['dataMin','dataMax']}
+                 tickFormatter={
+                   value => new Date(value).toLocaleDateString(locale)
+                 } />
+          <YAxis />
+          <Tooltip labelFormatter={
+                     value => new Date(value).toLocaleDateString(locale)
+                   }
+                   formatter={
+                     value => [numberFormat.format(value),"munaa"]
+                   } />
+        </LineChart>
+      </ResponsiveContainer>
+
+
     </div>
   )
 }
