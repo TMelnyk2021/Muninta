b735a7bd49859f8114986a31667b960924442f1f
TMelnyk2021 <t.melnyk2021@gmail.com>
2025-02-26 19:36:28 +0200

    lisää ympyräkaavion suorituksista - kanalasta

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,3 +1,5 @@
+import { LabelList, Legend, Pie, PieChart } from 'recharts'
+
 import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 import styles from './Stats.module.scss'
@@ -14,6 +16,19 @@ function Stats(props) {
       amount: item.m_amount
     })
   )
+
+  const reducer = (resultData, item) => {
+
+    const index = resultData.findIndex(arrayItem => arrayItem.type === item.type)
+    if (index >= 0) {
+      resultData[index].amount = resultData[index].amount + item.s_amount + item.m_amount + item.l_amount
+    } else {
+      resultData.push({type: item.type, amount: item.s_amount + item.m_amount + item.l_amount})
+    }
+    return resultData
+  }
+
+  const piedata = props.data.reduce(reducer, [])
 
 
   return (
@@ -41,6 +56,21 @@ function Stats(props) {
         </LineChart>
       </ResponsiveContainer>
 
+      <h3>Munien kerääminen kanalasta</h3>
+      <ResponsiveContainer height={350}>
+      <PieChart>
+          <Pie data={piedata} dataKey='amount' nameKey='type'>
+            <LabelList dataKey='amount' 
+                       position='inside' 
+                       formatter={
+                         value => numberFormat.format(value)
+                       } /> 
+          </Pie>
+          <Legend />
+          <Tooltip formatter={ value => numberFormat.format(value) } />
+        </PieChart>
+
+      </ResponsiveContainer>   
 
     </div>
   )
